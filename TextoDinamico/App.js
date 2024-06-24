d7427455c4482200397ca4b6bc23783a7d5b6cf5
CaioLopes18 <r9506505@gmail.com>
2024-06-24 00:27:25 -0300

    Atualização TextoDinamico

--- TextoDinamico/App.js
+++ TextoDinamico/App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { TextInput } from 'react-native-paper';
 export default function App() {
+  const [text,setText] = useState("")
   return (
     <View style={styles.container}>
       <Text>Testando</Text>
-      
+      <TextInput
+      label="Email"
+      value={text}
+      onChangeText={text => setText(text)}
+    />
     </View>
   );
 }
