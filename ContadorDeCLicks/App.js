26ce63d164fee00d0c282d32b18d98f148786226
CaioLopes18 <r9506505@gmail.com>
2024-06-23 21:48:21 -0300

    git commit -m "Atualização do ContadorDeCLicks"

--- ContadorDeCLicks/App.js
+++ ContadorDeCLicks/App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+/*
+**Objetivo:** Criar um contador que incrementa um valor cada vez que um botão é 
+pressionado.
+**Instruções:**
+*/
 
 export default function App() {
+  const [Count,setCount] = useState(0)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{Count}</Text>
+      <Button  mode="contained" onPress={() => setCount(Count+1)}>
+        Press me
+      </Button>
     </View>
   );
 }
