e9e39e90dbc30e4509aa19e68dd54409ff16e865
CaioLopes18 <r9506505@gmail.com>
2024-06-24 00:51:17 -0300

    git commit

--- TextoDinamico/App.js
+++ TextoDinamico/App.js
@@ -2,16 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TextInput } from 'react-native-paper';
+
+/* **Objetivo:** Criar um aplicativo que muda o texto exibido com base no que o usuário digitar.*/
+
 export default function App() {
   const [text,setText] = useState("")
   return (
     <View style={styles.container}>
-      <Text>Testando</Text>
-      <TextInput
-      label="Email"
-      value={text}
-      onChangeText={text => setText(text)}
-    />
+      <View>
+        <Text style={styles.textContainer}>Insira um texto:</Text>
+        <Text style={styles.textContainer}>{text}</Text>
+      </View>
+      <View>
+        <TextInput style={styles.inputContainer}
+          onChangeText={text => setText(text)}
+        />
+      </View>
+      
     </View>
   );
 }
@@ -23,4 +30,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textContainer:{
+    textAlign:"center",
+    margin:10,
+    fontSize:30
+  },
+  inputContainer:{
+    width:300,
+  }
 });
