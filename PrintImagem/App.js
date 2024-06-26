6a049932fde7e6ef84f1ce3181c0fdfdb7f1a4da
CaioLopes18 <r9506505@gmail.com>
2024-06-26 10:15:27 -0300

    Atualização PrintImagem

--- PrintImagem/App.js
+++ PrintImagem/App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native'; 
+import { StyleSheet, Text, View,Image } from 'react-native'; 
 
 /***Objetivo:** Adicionar uma imagem ao aplicativo.*/
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testando app</Text>
+      <Text style={{fontSize:20}}>
+        Inserindo uma imagem
+      </Text>
+      <Text>
+        ReactLogo
+      </Text>
+      <Image 
+      style={styles.imgContainer}
+      source={require("./Src/Images/ReactLogo.png")}
+      />
     </View>
   );
 }
@@ -18,4 +27,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imgContainer:{
+    marginTop:10,
+    width:100,
+    height:100,
+  }
 });
