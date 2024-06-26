cc0b5d43ff82f3a0c6f26f432ab94272ae3c0127
Caio Lopes <126980242+CaioLopes18@users.noreply.github.com>
2024-06-26 11:50:38 -0300

    Atualização EstilizacaoCondicional

--- EstilizacaoCondicional/App.js
+++ EstilizacaoCondicional/App.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Checkbox } from 'react-native-paper';
 import { useState } from 'react';
 
+/* *Objetivo:** Alterar a cor do texto com base em uma condição. */
+
 export default function App() {
 
   const [checked, setChecked] = useState(false);
