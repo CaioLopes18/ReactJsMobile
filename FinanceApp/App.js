3d363c9d1a9c5f22c231a2a6c7a99bdd0f90ee0e
CaioLopes18 <r9506505@gmail.com>
2024-07-03 12:02:07 -0300

    continuação do projeto finance app

--- FinanceApp/App.js
+++ FinanceApp/App.js
@@ -1,26 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import AddItem from './Add_item';
+import { StyleSheet,View,FlatList } from 'react-native';
+import * as React from 'react';
+import { useState } from 'react';
+import { Modal, Portal, Text, Button,Picker, PaperProvider, TextInput,List } from 'react-native-paper';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View>
-        <AddItem>
+  
+  const [visible, setVisible] = useState(false);
 
-        </AddItem>
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const containerStyle = {backgroundColor: 'white', padding: 20};
+
+  const [spentType,setSpentType] = useState("")
+  const [spentValue,setSpentValue] = useState(0)
+  const [spentDescription,setSpentDescription] = useState("")
+
+  const [list,setList] = useState([])
+
+  const addList =()=>{
+    tempList = {
+      "type":spentType,
+      "value":spentValue,
+      "description":spentDescription
+    }
+    const updatedItems = [...list, tempList]
+    setList(updatedItems)
+    hideModal()
+  }
+
+  return (
+    <PaperProvider >
+      <View style={styles.container}>
+        <View style={{width:"100%", backgroundColor:"#fff"}}>
+          <FlatList
+            data={list}
+            renderItem={({item})=>(
+              <List.Item
+                title={item.type}
+                description={item.description}
+                right={props =>
+                 <List.Item title={`${item.value}`}/>
+                }
+              />
+            )}
+          />
+        </View>
+        <View>
+          <Portal>
+            <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+              <TextInput onChangeText={Text=>setSpentType(Text)}/>
+              <TextInput 
+                onChangeText={
+                  Text=>setSpentValue(Text)
+                }
+              />
+              <TextInput
+                onChangeText={
+                  Text=>setSpentDescription(Text)
+                }
+              />
+              <Button
+              onPress={()=>{ 
+                addList()
+                }}>
+                Enviar
+              </Button>
+            </Modal>
+          </Portal>
+          <Button 
+          style={{marginLeft:-60}}
+            icon="plus"
+            buttonColor='orange'
+            onPress={showModal}
+           />
+        </View>
       </View>
-    </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width:"100%",
-    height:"100%"
+    backgroundColor: 'gray',
+    flexGrow:1,
+    alignItems:"flex-end",
+    flexDirection:"row"
   },
+
 });
